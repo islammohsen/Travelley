9f80dd9d152adc482df4e1f27187e32b7ec5588b
Raamyy <ramyeg26@gmail.com>
2018-04-13 13:05:18 +0200

    Implemented Ticket Class

--- Travelley/Ticket.cs
+++ Travelley/Ticket.cs
@@ -8,5 +8,25 @@ namespace Travelley
 {
     class Ticket
     {
+        private string TicketType;
+        private int NumberOfSeats;
+        private string SerialNumber;
+        private double Price;
+        private Trip TicketTrip;
+        
+        Ticket(string TicketType, int NumberOfSeats, string SerialNumber, double Price,Trip TicketTrip)
+        {
+            this.TicketType1 = TicketType;
+            this.NumberOfSeats1 = NumberOfSeats;
+            this.SerialNumber1 = SerialNumber;
+            this.Price1 = Price;
+            this.TicketTrip1 = TicketTrip;
+        }
+        
+        public string TicketType1 { get => TicketType; set => TicketType = value; }
+        public int NumberOfSeats1 { get => NumberOfSeats; set => NumberOfSeats = value; }
+        public string SerialNumber1 { get => SerialNumber; set => SerialNumber = value; }
+        public double Price1 { get => Price; set => Price = value; }
+        internal Trip TicketTrip1 { get => TicketTrip; set => TicketTrip = value; }
     }
 }
